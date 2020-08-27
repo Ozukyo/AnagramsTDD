4392e4233c8563baabf9ed6c4882933281af08ad
Ozukyo <ozukyo@gmail.com>
2020-08-27 15:02:55 +0200

    Add two more tests - isAnagram

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ module.exports = {
     parseDataToArray: function(filename) {
         let text = fs.readFileSync(filename).toString("utf-8");
         return text.split("\n");
+    },
+
+    isAnagram: function(a, b) {
+        let firstWord = a.toLowerCase().split("").sort().join("");
+        let secondWord = b.toLowerCase().split("").sort().join("");
+        console.log(a, b);
+
+        return firstWord === secondWord;
     }
 }
 
--- test/appTest.js
+++ test/appTest.js
@@ -4,8 +4,23 @@ const app = require("../app");
 describe("app", function() {
     describe("parseDataToArray", function() {
         it("should return array", function () {
-            assert.typeOf(app.parseDataToArray("wordlist.txt"), "array");
+            let result = app.parseDataToArray("wordlist.txt");
+            assert.typeOf(result, "array");
         });
 
     });
+
+    describe("isAnagram", function() {
+        let array = app.parseDataToArray("wordlist.txt");
+
+        it("should return false", function () {
+            let result = app.isAnagram(array[10], array[13]);
+            assert.isFalse(result, "false");
+        });
+
+        it("should return true", function () {
+            let result = app.isAnagram(array[207804], array[140451]);
+            assert.isTrue(result, "true");
+        })
+    })
 });
