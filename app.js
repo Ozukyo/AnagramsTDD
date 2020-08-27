9e3b6837776ebab85ed542c3e03ae96763512f45
Ozukyo <ozukyo@gmail.com>
2020-08-27 17:57:21 +0200

    Implement last tests

--- app.js
+++ app.js
@@ -9,9 +9,34 @@ module.exports = {
     isAnagram: function(a, b) {
         let firstWord = a.toLowerCase().split("").sort().join("");
         let secondWord = b.toLowerCase().split("").sort().join("");
-        console.log(a, b);
 
         return firstWord === secondWord;
+    },
+
+    sortAnagramsToArrays: function(arrayOfWords) {
+        let listOfAllArraysOfAnagrams = [];
+
+        arrayOfWords.forEach((word) => {
+            let listOfAnagrams = [];
+            listOfAnagrams.push(word);
+
+            for(let i = 0; i < arrayOfWords.length; i++) {
+                if(arrayOfWords[i] === word) {
+                    continue;
+                }
+                if(this.isAnagram(arrayOfWords[i], word)) {
+                    listOfAnagrams.push(arrayOfWords[i]);
+                }
+            }
+            listOfAnagrams.sort();
+            listOfAllArraysOfAnagrams.push(listOfAnagrams);
+
+        });
+
+
+        console.log(listOfAllArraysOfAnagrams.filter(( t={}, a=> !(t[a]=a in t) )).length);
+        return listOfAllArraysOfAnagrams.filter(( t={}, a=> !(t[a]=a in t) ));
     }
 }
+
 
--- test/appTest.js
+++ test/appTest.js
@@ -2,7 +2,9 @@ const assert = require("chai").assert;
 const app = require("../app");
 
 describe("app", function() {
+
     describe("parseDataToArray", function() {
+
         it("should return array", function () {
             let result = app.parseDataToArray("wordlist.txt");
             assert.typeOf(result, "array");
@@ -21,6 +23,36 @@ describe("app", function() {
         it("should return true", function () {
             let result = app.isAnagram(array[207804], array[140451]);
             assert.isTrue(result, "true");
-        })
-    })
+        });
+    });
+
+    describe("sortAnagramToArrays", function() {
+        let array = app.parseDataToArray("test.txt");
+        let resultArr = app.sortAnagramsToArrays(array);
+
+        it("should return array", function() {
+            assert.typeOf(resultArr, "array");
+        });
+
+        it("should not be empty", function() {
+            assert.isNotEmpty(resultArr, "not empty");
+        });
+
+        it("should have 36 elements", function() {
+            let resultLength = resultArr.length;
+            assert.equal(resultLength, 36);
+        });
+
+        it("should second inner array have 4 elements", function() {
+            let resultLength = resultArr[1].length;
+            assert.equal(resultLength, 4);
+        });
+
+        it("should have less than 50 elements", function() {
+            let resultLength = resultArr.length;
+            assert.isBelow(resultLength, 50);
+        });
+    });
+
+
 });
